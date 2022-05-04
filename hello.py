ad930a916ed044f20edec70df1937281e87516ad
Katie Gaertner <katie.gaertner@gmail.com>
2022-05-04 16:52:49 +0000

    repair flask

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from flask_restful import Api
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hi, Jon! Hi, Ben!"
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+def hello():
+    return 'Hello World! Hi, Jon! Hi, Ben!'
