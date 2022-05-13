6ccf44157508b71473efd12831a427cf175e268d
Katie Gaertner <katie.gaertner@gmail.com>
2022-05-13 15:56:36 +0000

    adding forecast

--- hello.py
+++ hello.py
@@ -1,11 +1,24 @@
 from flask import Flask
-#from flask_restful import Api
+from flask_restful import Api
+from google.cloud import bigquery
+
 
 app = Flask(__name__)
 
+api = Api(app)
+client = bigquery.Client()
+query = """
+        SELECT time_series_timestamp AS date, time_series_data AS forecast 
+        FROM `gaertnergcp.chicago_crimes.Daily_MVT_Forecast`
+        WHERE time_series_timestamp = '2022-06-01 00:00:00 UTC'
+        """
+
+query_job = client.query(query)
+df = query_job.to_dataframe
 
 @app.route('/')
-def hello():
-    return 'Hello World! Hi, Jon! Hi, Ben!'
+def query():
+    response = df
+    return response
 
 #I changed something else
