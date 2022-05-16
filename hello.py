c0058418ce2acefb24aa52162b28af6d13831e29
Katie Gaertner <katie.gaertner@gmail.com>
2022-05-16 17:23:27 +0000

    remove restful to fix linting

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from flask_restful import Api
-
+#from flask_restful import Api
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello, Maggie" 
+
+#if __name__ == "__main__":
+#    app.run(port=8080)
