b31a934a29a5942c635155e846aeb95bb0feb28a
Katie Gaertner <katie.gaertner@gmail.com>
2022-05-17 21:38:57 +0000

    forecast to url

--- demo.py
+++ demo.py
@@ -1,25 +1,27 @@
-import flask
+from flask import Flask
+from flask_restful import Api
 from google.cloud import bigquery
 
-bigquery_client = bigquery.Client()
+app = Flask(__name__)
+api = Api(app)
 
-app = flask.Flask(__name__)
-@app.route("/")
-def main():
-    # Define query_job and query object
-    query_job = bigquery_client.query(
-        """
+client = bigquery.Client()
+
+query = """
         SELECT ROUND(time_series_data) AS forecast 
         FROM `gaertnergcp.chicago_crimes.Daily_MVT_Forecast`
         WHERE time_series_timestamp = '2022-06-01 00:00:00 UTC'
         """
-    )
-   
+
+query_job = client.query(query)
+
+@app.route('/', methods=['GET'])
+def demo():
     # Handle query_job result and return to flask to display
     res = query_job.result()
     for row in res:
-        output = "Forecast for Motor Vehicle Thefts in Chicago on June 1: " + str(row[0])
+        output = "Forecast for Motor Vehicle Thefts in Chicago on June 1, 2022: " + str(row[0])
         return output
 
-if __name__ == "__main__":
-    app.run(port=8080)
+#if __name__ == "__main__":
+#    app.run(port=8080)
